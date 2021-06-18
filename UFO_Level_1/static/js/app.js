982edcfa40f180a608ed8b403a229e184db2dd92
rperezme <78776114+rperezme-data@users.noreply.github.com>
2021-06-18 00:57:04 -0500

    Test Basic Filters

--- UFO_Level_1/static/js/app.js
+++ UFO_Level_1/static/js/app.js
@@ -2,3 +2,33 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+
+// Set initial values
+var datetime = "1/1/2010";
+var city = "el cajon";
+
+
+// Search datetime 
+function datetimeQuery(row) {
+    return row.datetime === datetime;
+}
+
+// Search city 
+function cityQuery(row) {
+    return row.city === city;
+}
+
+
+
+// Filter datetime
+var results1 = data.filter(datetimeQuery);
+console.log("\nFilter datetime")
+results1.forEach(result => console.log(result))
+
+// Filter city
+var results2 = data.filter(cityQuery);
+console.log("\nFilter city")
+results2.forEach(result => console.log(result))
+
+
+// console.log(results);
