9fdd528b8ad4eeae9815fc6b59e12e2493d38bf0
rperezme <78776114+rperezme-data@users.noreply.github.com>
2021-06-19 23:54:29 -0500

    Add Event Handlers

--- UFO_Level_1/static/js/app.js
+++ UFO_Level_1/static/js/app.js
@@ -1,34 +1,65 @@
-// from data.js
+
+// LOAD TABLE DATA
+// Assign the data from `data.js` to a descriptive variable
 var tableData = data;
 
-// YOUR CODE HERE!
+// SELECT ELEMENTS
+var form = d3.select("form");
+var datetimeForm = d3.select("#datetime");
+var button = d3.select("#filter-btn");
 
-// Set initial values
-var datetime = "1/1/2010";
-var city = "el cajon";
+// EVENT HANDLERS
+form.on("submint", runFilter);
+datetimeForm.on("change", runFilter);
+button.on("click", runFilter);
 
 
-// Search datetime 
 function datetimeQuery(row) {
-    return row.datetime === datetime;
-}
-
-// Search city 
-function cityQuery(row) {
-    return row.city === city;
+    return row.datetime === datetimeInput;
 }
 
 
+// FILTER FUNCTION
+function runFilter() {
 
-// Filter datetime
-var results1 = data.filter(datetimeQuery);
-console.log("\nFilter datetime")
-results1.forEach(result => console.log(result))
+    function datetimeQuery(row) {
+        return row.datetime === datetimeInput;
+    }
 
-// Filter city
-var results2 = data.filter(cityQuery);
-console.log("\nFilter city")
-results2.forEach(result => console.log(result))
+    // Prevent the page from refreshing
+    d3.event.preventDefault();
+
+    // Get the value property of the input elements
+    var datetimeInput = datetimeForm.property("value");
+
+    var results = tableData.filter(datetimeQuery);
+    console.log(results);
+
+}
+
+
+
+
+// // Search datetime 
+// function datetimeQuery(row) {
+//     return row.datetime === datetime;
+// }
+
+// // Search city 
+// function cityQuery(row) {
+//     return row.city === city;
+// }
+
+
+// // Filter datetime
+// var results1 = data.filter(datetimeQuery);
+// console.log("\nFilter datetime")
+// results1.forEach(result => console.log(result))
+
+// // Filter city
+// var results2 = data.filter(cityQuery);
+// console.log("\nFilter city")
+// results2.forEach(result => console.log(result))
 
 
 // console.log(results);
