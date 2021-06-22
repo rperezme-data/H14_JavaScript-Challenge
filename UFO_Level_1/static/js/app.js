67e96868c751ad959bed4ae28b18d122c815a66f
rperezme <78776114+rperezme-data@users.noreply.github.com>
2021-06-22 02:53:04 -0500

    Add No-Results-Found Filter

--- UFO_Level_1/static/js/app.js
+++ UFO_Level_1/static/js/app.js
@@ -3,82 +3,106 @@
 // Assign data to descriptive variable
 var tableData = data;
 
-// RENDER TABLE (index.html)
-// Get reference to table body <tbody>
-var tbody = d3.select("tbody");
-
-// Loop tableData rows
-tableData.forEach((element) => {
-    // Append one <tr> (table row) for each element (data row)
-    var row = tbody.append("tr");
-    // Loop tableData columns
-    Object.entries(element).forEach(([key, value]) => {
-        // Append one <td> (table cell) per element value (data column)
-        row.append("td").text(value);
-    })
-});
+// EXPLORE TABLE DATA
+// Get range limits (min, max)
+// View unique values
 
 
 // SELECT ELEMENTS
-var form = d3.select("form");
-var datetimeForm = d3.select("#datetime");
-var button = d3.select("#filter-btn");
+// Get table body <tbody> reference
+var tbody = d3.select('tbody');
+
+// Get forms & buttons reference
+var form = d3.select('form');
+var datetimeForm = d3.select('#datetime');
+var button = d3.select('#filter-btn');
+
 
 // EVENT HANDLERS
-form.on("submit", runFilter);
-datetimeForm.on("change", runFilter);
-button.on("click", runFilter);
+form.on('submit', runFilter);
+datetimeForm.on('change', runFilter);
+button.on('click', runFilter);
+
+
+// RENDER COMPLETE TABLE (index.html)
+// Loop tableData rows
+tableData.forEach((element) => {
+    // Append one <tr> (table row) for each element (data row)
+    var row = tbody.append('tr');
+    // Loop tableData columns
+    Object.entries(element).forEach(([key, value]) => {
+        // Append one <td> (table cell) per element value (data column)
+        row.append('td').text(value);
+    })
+});
+
 
 
 function datetimeQuery(row) {
     return row.datetime === datetimeInput;
 }
 
+// var results = tableData.filter(datetimeQuery);
+//     console.log(results);
+
+
 
 // FILTER FUNCTION
 function runFilter() {
 
-    // Debug
-    console.log("Enter filter function");
+    
+    console.log("Enter filter function");  // DEBUG MSG
+
 
     // Prevent the page from refreshing
     d3.event.preventDefault();
 
-    // Get the value property of the input elements
-    var datetimeInput = datetimeForm.property("value");
+    // Get input values (from elements)
+    var datetimeInput = datetimeForm.property('value');
 
-    function datetimeQuery(row) {
-        return row.datetime === datetimeInput;
+    // Check input (out-of-range values)
+    if (tableData.includes("datetime", datetimeInput)) {
+        console.log("OK");
+    } else {
+        console.log("Search value is out-of-range");
     }
 
 
+    // GET RESULTS FROM SEARCH CRITERIA
+    var results = tableData.filter((row) => row.datetime === datetimeInput);
 
-    var results = tableData.filter(datetimeQuery);
-    console.log(results);
+  
+    console.log(results);  // DEBUG MSG
 
-    // a) delete table
-    // b) if no-value
-    // c) if not-found...
-    
 
-    // RENDER RESULTS TABLE
+    // RENDER TO HTML
     // Clear previous rendered table
-    tbody.html("")
-    
-    // Loop tableData rows
-    results.forEach((element) => {
-        // Append one <tr> (table row) for each element (data row)
-        var row = tbody.append("tr");
-        // Loop tableData columns
-        Object.entries(element).forEach(([key, value]) => {
-            // Append one <td> (table cell) per element value (data column)
-            row.append("td").text(value);
-        })
-    });
+    tbody.html("");
+
+    // Check results (no-results-found)
+    if (results.length === 0) {
+
+        // RENDER ERROR MESSAGE
+        tbody.append('p').text("No results found for your search criteria");
+        console.log("No results found for your search criteria");
+
+    } else {
+        
+        // RENDER RESULTS TABLE
+        // Loop tableData rows
+        results.forEach((element) => {
+            // Append one <tr> (table row) for each element (data row)
+            var row = tbody.append('tr');
+            // Loop tableData columns
+            Object.entries(element).forEach(([key, value]) => {
+                // Append one <td> (table cell) per element value (data column)
+                row.append('td').text(value);
+            })
+        });
+    }
 
     
-    // Debug
-    console.log("Exit filter function");
+    console.log("Exit filter function"); // DEBUG MSG
 
 }
 
